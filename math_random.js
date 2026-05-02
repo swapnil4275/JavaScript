22fd7d086c5bbe821fd877f234031175c4f602b8
swapnil4275 <swapnilpatil4275@gmail.com>
2026-05-02 13:05:30 +0530

    add files

---
+++ math_random.js
@@ -0,0 +1,22 @@
+// Demonstrate Math Random 
+
+
+// range = 0 to <1
+console.log(Math.random() );
+
+
+// range = 0 to <10
+console.log(Math.floor(Math.random() * 10));
+
+
+// range = 0 to 10 (include 10)
+console.log(Math.floor(Math.random() * 10) + 1);
+
+
+
+// range = 0 to <100
+console.log(Math.floor(Math.random() * 100));
+
+
+// range = 0 to <1000
+console.log(Math.floor(Math.random() * 1000));
